aa8f592283ecb0f6c3efc82bf567dfc8f1c1c9c9
angsori123 <156868540+angsori123@users.noreply.github.com>
2024-01-17 09:28:08 +0700

    Membaut file biodata.py
    berisi kode input biodata

---
+++ biodata.py
@@ -0,0 +1,8 @@
+name = input("masukan nama : ")
+umur = input("masukan umur : ")
+alamat = input("masukan alamat : ")
+
+print("\n---Biodata---")
+print("Nama : " + nama)
+print("Umur : " + umur)
+print("Alamat : " + alamat)
